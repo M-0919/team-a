962647bd269954aaae1d4aef400ab9f3ee6e59d5
Alex Kayser <akctba@gmail.com>
2020-06-11 11:49:48 -0700

    paragraph width and text

--- src/components/excerptFromFacebook/excerptFromFacebook.jsx
+++ src/components/excerptFromFacebook/excerptFromFacebook.jsx
@@ -2,13 +2,10 @@ import React from "react"
 import "./excerptFromFacebook.scss"
 const excerptFromFacebook = () => (
   <div className="excerpt">
-    <h2 className="excerpt__title">Excerpt from Facebook</h2>
+    <h2 className="excerpt__title">Excerpt</h2>
     <p className="excerpt__content">
-      Morbi id nisi eu turpis vehicula laoreet. Cras ac diam velit. Suspendisse
-      potenti. Proin iaculis elit a neque imperdiet lacinia. Nullam id
-      ullamcorper massa. Lorem ipsum dolor sit amet, consectetur adipiscing
-      elit. Curabitur posuere metus eu eros congue, ac mollis mauris cursus.
-      Vivamus condimentum mi non urna tincidunt venenatis.
+    As I reflected on the completion of my manuscript on my life with Parkinson’s disease, I asked myself a question whether, given the choice, I would have chosen to have it. Who would want to be diagnosed with a chronic, neurodegenerative illness, for which there is no cure? Who would want to experience a continuous tremor in one hand, and a dropped foot making walking difficult? Who would want to experience anxiety, depression and insomnia? Who would want to have cognitive impairment making communication difficult at times?
+    Over the past ten years I have experienced many of these symptoms so I do feel that I have enough authority to have asked that question. And, as strange as it may seem, I came to the conclusion that I would have chosen to have it. Let me explain why.
     </p>
   </div>
 )
