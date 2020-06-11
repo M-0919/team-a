ee604f903e4a020879ba88838544c54637532e35
Alex Kayser <akctba@gmail.com>
2020-06-11 16:18:08 -0700

    placeholder trailer

--- src/components/bookTrailer/bookTrailer.jsx
+++ src/components/bookTrailer/bookTrailer.jsx
@@ -2,7 +2,14 @@ import React from "react"
 import "./bookTrailer.scss"
 
 const BookTrailer = () => (
-  <div className="bookTrailer">booktrailer Component here</div>
+  <div className="bookTrailer">
+
+    <div className="trailer">
+      <iframe width="720" height="405" src="https://www.youtube.com/embed/y2Ky3Wo37AY?controls=0" frameborder="0" allow="autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+
+
+  </div>
 )
 
 export default BookTrailer
