d6a4713a696cefe73e4f90240dae2a6ad1ca62a2
Bianca <biancacmatira@gmail.com>
2020-06-12 03:07:06 -0700

    Fixed bugs in iframe

--- src/components/bookTrailer/bookTrailer.jsx
+++ src/components/bookTrailer/bookTrailer.jsx
@@ -9,9 +9,9 @@ const BookTrailer = () => (
         width="720"
         height="405"
         src="https://www.youtube.com/embed/y2Ky3Wo37AY?controls=0"
-        frameborder="0"
+        frameBorder="0"
         allow="autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
+        allowFullScreen
       ></iframe>
     </div>
   </div>
