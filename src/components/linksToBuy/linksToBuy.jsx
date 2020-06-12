99338aea262fcd762fd52eecba8d1fc831f5a304
Alex Kayser <akctba@gmail.com>
2020-06-11 19:13:48 -0700

    icons links

--- src/components/linksToBuy/linksToBuy.jsx
+++ src/components/linksToBuy/linksToBuy.jsx
@@ -1,25 +1,29 @@
 import React from "react"
 import "./linkToBuy.scss"
 import { Link } from "gatsby"
+import amazonIcon from "../../images/amazon-icon.png"
+import googleIcon from "../../images/play-icon.png"
 
 const LinkToBuy = () => (
   <div className="links">
     <div className="links__content">
-      <p className="links__text">Purchase your copy here!</p>
+      <p className="links__text">Where you can find the book</p>
       <ul className="links__list">
         <li>
           <Link to="/" className="links__button">
-            Buy
+            <img className="button__icon" alt="amazon" src={amazonIcon} />
+            Amazon
           </Link>
         </li>
         <li>
           <Link to="/" className="links__button">
-            Buy
+            <img className="button__icon" alt="Google Books" src={googleIcon} />
+            Google Play
           </Link>
         </li>
         <li>
           <Link to="/" className="links__button">
-            Buy
+            Book store
           </Link>
         </li>
       </ul>
